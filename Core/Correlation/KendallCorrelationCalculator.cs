52ee5bcce0c856d0c3c61d60c9912eecc55c1744
Garderik <scream.mode@gmail.com>
2018-04-12 14:53:54 +0400

    Make KendallCorrelationCalculator implementation. Close #8

--- Core/Correlation/KendallCorrelationCalculator.cs
+++ Core/Correlation/KendallCorrelationCalculator.cs
@@ -1,12 +1,25 @@
-﻿using System;
-
-namespace Core.Correlation
+﻿namespace Core.Correlation
 {
-    public class KendallCorrelationCalculator : CorrelationCalculator
+    internal class KendallCorrelationCalculator : CorrelationCalculator
     {
         public override double GetCorrelation(Function f1, Function f2)
         {
-            throw new NotImplementedException();
+            Scale(f1, f2);
+
+            double R = 0;
+            for (int i = 0; i < f1.Count - 1; i++)
+            {
+                for (int j = i + 1; j < f1.Count; j++)
+                {
+                    bool inversion = f1[i].Y < f1[j].Y && f2[i].Y >= f2[j].Y;
+                    R += inversion ? 1 : 0;
+                }
+            }
+
+            double result = 4 * R / (f1.Count * (f1.Count - 1));
+            result = 1 - result;
+
+            return result;
         }
     }
 }
